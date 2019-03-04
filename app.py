f4ba8636b13c363abf2d1c8aca1c9483547cc0a9
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-03-04 22:52:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
 from flask import Flask, render_template, request
 from redis import Redis
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
 import os
 import markdown
 
 app = Flask(__name__)
 redis = Redis(host='redis',port=5000)
 
+#from wtforms.validators import DataRequired
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    age = InterField('age', validators=[DataRequired()])
+
 # index.html vaizdas
-@app.route('/', methods=["GET"])
+@app.route('/', methods=["GET", "POST"])
 def home():
-        return render_template('home.html')
+    form = MyForm(request.form)
+ 
+    print (form.errors)
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        print (name, age)
+    return render_template('home.html')
 
 @app.route("/tasks", methods=["GET", "POST"])
 def tasks():
-        if request.method == 'POST':
-                return 'All tasks here'
+    if request.method == 'POST':
+        return 'All tasks here'
 
 #def index():
 #        indFile = open('README.md', 'r')
@@ -22,4 +37,4 @@ def tasks():
 #        return markdown.markdown(content)
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0",debug=True)
