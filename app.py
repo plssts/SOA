9ca996194bf16332e994bf16ad2ce7c017caf2ff
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-03-04 20:25:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from redis import Redis
 import os
+import markdown
 
 app = Flask(__name__)
 redis = Redis(host='redis',port=5000)
 
 @app.route('/')
-def hello():
-        return "Hello"
+def index():
+        indFile = open('README.md', 'r')
+        content = indFile.read()
+        return markdown.markdown(content)
 
 if __name__ == "__main__":
         app.run(host="0.0.0.0",debug=True)
