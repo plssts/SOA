f4a597de51208d6e8ffe23bd3e584113f6105281
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-03-04 20:16:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-print("Hello")
+from flask import Flask
+from redis import Redis
+import os
+
+app = Flask(__name__)
+redis = Redis(host='redis',port=5000)
+
+@app.route('/')
+def hello():
+        return "Hello"
 
 if __name__ == "__main__":
         app.run(host="0.0.0.0",debug=True)
