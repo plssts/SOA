cbd371d1920a1bafdb7765761b49bcbb9da31305
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-03-05 21:02:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,17 @@
 from flask import Flask, render_template, request
-from redis import Redis
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from flask_restful import Api
 import os
 import markdown
 
 app = Flask(__name__)
-redis = Redis(host='redis',port=5000)
+progInterface = Api(app)
 
-#from wtforms.validators import DataRequired
-
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[])
-
-# index.html vaizdas
+# index vaizdas
 @app.route('/', methods=["GET", "POST"])
 def home():
     return markdown.markdown(open('README.md', 'r').read())
 
-@app.route("/tasks", methods=["GET", "POST"])
-def tasks():
-    if request.method == 'GET':
-        return 'All tasks here'
-    return 'all tasks here'
-
-#def index():
-#        indFile = open('README.md', 'r')
-#        content = indFile.read()
-#        return markdown.markdown(content)
+api.add_resource(NotesList, '/notes')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
