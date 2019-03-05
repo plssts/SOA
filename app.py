9f165985c21117be165c696333309735250e3b8f
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-03-05 21:54:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,20 +14,5 @@ def home():
 
 progInterface.add_resource(NotesList, '/notes')
 
-# duombazes uzkrovimas
-def initDatabase():
-    if 'db' not in g:
-        g.db = connect_to_database()
-
-    return g.db
-
-# duombazes panaikinimas
-@app.teardown_appcontext
-def teardown_db():
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
