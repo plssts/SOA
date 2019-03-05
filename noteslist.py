712bfe59f5dd1d44fe444b36bfa3989af0947f36
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-03-05 21:06:21 +0200

    Create noteslist.py

---
+++ noteslist.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, reqparse
+import shelve
+
+app = Flask(__name__)
+
+class NotesList(Resource):
+    def get(self):
+        entries = get_db()
+        elements = list(entries.keys())
+
+        notes = [entries[e] for e in elements]
+
+        return {'message': 'Success', 'data': notes}, 200
