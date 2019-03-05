17256e3e05c9e6171abf60b81bdb67c12a58e2ef
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-03-05 21:54:56 +0200

    Update noteslist.py

--- noteslist.py
+++ noteslist.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, g
 from flask_restful import Resource, reqparse
 import shelve
 
@@ -6,10 +6,24 @@ app = Flask(__name__)
 
 class NotesList(Resource):
     def get(self):
-        entries = get_db()
+        entries = initDatabase()
         elements = list(entries.keys())
 
         notes = [entries[e] for e in elements]
 
         return {'message': 'Success', 'data': notes}, 200
     
+# duombazes uzkrovimas
+def initDatabase():
+    if 'db' not in g:
+        g.db = connect_to_database()
+
+    return g.db
+
+# duombazes panaikinimas
+@app.teardown_appcontext
+def teardown_db():
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
