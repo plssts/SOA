6302e20b3a31f22c3dc1395ccf2a4a1d1714b507
krsi3832 <budorietis@gmail.com>
2019-03-13 14:48:43 +0200

    Update __init__.py

--- members_registry/__init__.py
+++ members_registry/__init__.py
@@ -145,7 +145,7 @@ class NamesList(Resource):
 api.add_resource(MembersList, '/members')
 api.add_resource(Member, '/members/<string:phone>')
 api.add_resource(NamesList, '/names')
-api.add_resource(Name, '/names/<string:name>')#pakeitimas 2
+api.add_resource(Name, '/names/<string:name>')
 
 
 
