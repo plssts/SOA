24d08147f569b51469e0fe105d1044088439798d
krsi3832 <budorietis@gmail.com>
2019-03-13 14:08:48 +0200

    Update __init__.py

--- members_registry/__init__.py
+++ members_registry/__init__.py
@@ -126,10 +126,10 @@ class NamesList(Resource):#pakeitimas2
         kazkas = [shelve[i] for i in shelf]
         members = []
 
-        for i in shelf:
+       # for i in shelf:
             
             #if shelve[i].name.cointais(let)
-            if "Kristupas" in shelve[i].name: 
+            if "Kristupas" in shelve.name: 
                 return {'message': 'Member found', 'data': shelve[i]}, 200
         
 
