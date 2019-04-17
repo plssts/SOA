b1fd48e484e6fb6b4b890f6ee2a6836b4b0e7211
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-04-17 23:23:49 +0300

    +

--- 2/app.py
+++ 2/app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, request, g
-from flask_restful import Api
+from flask import Flask, render_template, request, g, jsonify
+from flask_restful import Api, Resource, reqparse
 import os
 import markdown
+import requests
+import json
 
 app = Flask(__name__)
 progInterface = Api(app)
+
+class Conference(Resource):
+    def get(self):
+        headers = {'Accept': 'application/json'}
+        r = requests.get('http://usr:5009/users', headers=headers)
+        return json.dumps(r.text)
 
 # index vaizdas
 @app.route('/', methods=["GET", "POST"])
 def home():
     return markdown.markdown(open('README.md', 'r').read())
+
+progInterface.add_resource(Conference, '/c')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
