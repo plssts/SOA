c6a9d0133dfb07c783130f81bbc30633162381a2
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-04-24 14:08:21 +0300

    Update conferenceattendees.py

--- 2/conferenceattendees.py
+++ 2/conferenceattendees.py
@@ -28,7 +28,7 @@ class ConferenceAttendees(Resource):
         if args['email'] in previous:
             return {'message': 'This member is already participating', 'data': args['email']}, 409
         
-        r = requests.get('http://usr:5009/users/' + args['email'])
+        r = requests.get('http://usr_s:5009/users/' + args['email'])
         if r.status_code == 404:
             return {'message': 'No such member', 'data': args['email']}, 404
         
