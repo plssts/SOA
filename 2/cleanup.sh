d92621e57f554fae1679f16391ef30ac5756c805
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-04-24 14:07:45 +0300

    Update cleanup.sh

--- 2/cleanup.sh
+++ 2/cleanup.sh
@@ -5,5 +5,7 @@ docker stop base_s
 #echo "Removing user-based network bridge"
 #docker network rm ws_bridge
 echo "Clearing empty images"
+docker container rm usr_s
+docker container rm base_s
 docker rmi --force $(docker images --filter dangling=true -q --no-trunc)
 echo "Done"
