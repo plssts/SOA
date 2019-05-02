5e9720c84818531319149ed8897fc7ae2cb7faf8
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-05-02 11:51:42 +0300

    Update User.py

--- 2/WebServices/Pirmas/User.py
+++ 2/WebServices/Pirmas/User.py
@@ -29,6 +29,7 @@ class UserList(Resource):
         if args['email'] in shelf[str(cid)]:
             return {'message': 'Email Already Exists', 'data': {}}, 409
     
+        return {'message': args}, 200
         previous[args['email']] = args
         return {'message': previous}, 200
         shelf[str(cid)] = previous
