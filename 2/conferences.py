a7e39f28735dfbe71d50a6e51ad5643861c36dc2
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-05-02 19:57:39 +0300

    Update conferences.py

--- 2/conferences.py
+++ 2/conferences.py
@@ -21,35 +21,6 @@ class Conferences(Resource):
                 dataHash['attendees'] = [key for key in list(r.json()['data'].keys())]
 
         return {'message': 'Conference', 'data': dataHash}, 200
-    
-    def post(self, cid):
-        entries = database()
-        
-        if not (str(cid) in entries):
-            return {'message': 'No such conference', 'data': {}}, 404
-        
-        if 'embedded' in request.args:
-            previous = shelve.open('attendees.db')[str(cid)]['attendees']
-            
-            parser = reqparse.RequestParser()
-            parser.add_argument('email', required=True)
-            args = parser.parse_args()
-        
-            if args['email'] in previous:
-                return {'message': 'This member is already participating', 'data': args['email']}, 409
-        
-            r = requests.get('http://usr_s:5009/users/' + args['email'])
-            if r.status_code == 404:
-                return {'message': 'No such member', 'data': args['email']}, 404
-            
-            args['cid'] = str(cid)
-            previous.append(args['email'])
-            args['attendees'] = previous
-            email = args.pop('email', None)
-            shelve.open('attendees.db')[args['cid']] = args
-            return {'message': 'New attendee added', 'data': email}, 201
-        else:
-            return {'message': 'POST on /conferences/cid is for adding attendees only', 'data': {}}, 405
         
     def put(self, cid):
         entries = database()
@@ -70,27 +41,6 @@ class Conferences(Resource):
         
     def delete(self, cid):
         entries = database()
-
-        if 'embedded' in request.args:
-            previous = shelve.open('attendees.db')[str(cid)]['attendees']
-            
-            parser = reqparse.RequestParser()
-            parser.add_argument('email', required=True)
-            args = parser.parse_args()
-        
-            r = requests.get('http://usr_s:5009/users/' + args['email'])
-            if r.status_code == 404:
-                return {'message': 'No such member', 'data': args['email']}, 404
-            
-            if not args['email'] in previous:
-                return {'message': 'This member is not attending the conference', 'data': args['email']}, 404
-            
-            args['cid'] = str(cid)
-            previous.remove(args['email'])
-            args['attendees'] = previous
-            email = args.pop('email', None)
-            shelve.open('attendees.db')[args['cid']] = args
-            return {'message': 'Attendee removed', 'data': email}, 200
 
         if not (str(cid) in entries):
             return {'message': 'No such conference', 'data': {}}, 404
