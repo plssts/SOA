e09dd73fb3a49a45093f8013ff8872f65b641925
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-07-10 00:39:30 +0300

    Update GetConferenceResponse.java

--- 3/SOAP/src/main/java/com/soap/ws/jaxws/GetConferenceResponse.java
+++ 3/SOAP/src/main/java/com/soap/ws/jaxws/GetConferenceResponse.java
@@ -1,6 +1,5 @@
 package com.soap.ws.jaxws;
 import com.soap.ws.Conference;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
