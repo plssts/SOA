d2e011d0093d8c88f72eeecae5df775c71c758ba
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-07-10 00:18:04 +0300

    Update ConferenceEMB.java

--- 3/SOAP/src/main/java/com/soap/ws/ConferenceEMB.java
+++ 3/SOAP/src/main/java/com/soap/ws/ConferenceEMB.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.soap.ws;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -13,5 +8,4 @@ public class ConferenceEMB {
     public String date;
     public int cid;
     public Attendee[] attendees;
-    //public String[] attendees;
 }
