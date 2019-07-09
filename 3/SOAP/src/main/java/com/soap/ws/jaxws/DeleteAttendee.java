3004e5498cff418c6053f31c88707029daabeadf
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-07-10 00:13:04 +0300

    Update DeleteAttendee.java

--- 3/SOAP/src/main/java/com/soap/ws/jaxws/DeleteAttendee.java
+++ 3/SOAP/src/main/java/com/soap/ws/jaxws/DeleteAttendee.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.soap.ws.jaxws;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-/**
- *
- * @author LAPTOPELIS
- */
+
 @XmlRootElement(name = "deleteAttendee", namespace = "http://ws.soap.com/")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "deleteAttendee", namespace = "http://ws.soap.com/")
