a7b172526fc4654e285c9e6762f7fdcb68d043cc
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-07-10 00:18:18 +0300

    Update Conference.java

--- 3/SOAP/src/main/java/com/soap/ws/Conference.java
+++ 3/SOAP/src/main/java/com/soap/ws/Conference.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.soap.ws;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -12,6 +7,5 @@ public class Conference {
     public String info;
     public String date;
     public int cid;
-    //public Attendee[] attendees;
     public String[] attendees;
 }
