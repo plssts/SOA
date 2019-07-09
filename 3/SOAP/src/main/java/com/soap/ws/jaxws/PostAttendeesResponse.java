1b78342b5ab492c8df431446d38e338e16f29dbf
Paulius Staišiūnas <36136289+plssts@users.noreply.github.com>
2019-07-10 00:21:17 +0300

    Update PostAttendeesResponse.java

--- 3/SOAP/src/main/java/com/soap/ws/jaxws/PostAttendeesResponse.java
+++ 3/SOAP/src/main/java/com/soap/ws/jaxws/PostAttendeesResponse.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.soap.ws.jaxws;
 
 import com.soap.ws.OutcomeAttendee;
@@ -14,13 +9,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-
 @XmlRootElement(name = "postAttendeesResponse", namespace = "http://ws.soap.com/")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "postAttendeesResponse", namespace = "http://ws.soap.com/")
 public class PostAttendeesResponse {
     @XmlElement(name = "return", namespace = "")
-    //@XmlJavaTypeAdapter(.class)
     private OutcomeSimple _return;
 
     public OutcomeSimple getReturn() {
